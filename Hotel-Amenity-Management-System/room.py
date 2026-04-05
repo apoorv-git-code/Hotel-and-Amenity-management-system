c24c40787c0335bc7ab62b3aefba8d8e9d0058e9
Hardik1011Bansal <bansalsahab1011@gmail.com>
2026-04-06 00:48:25 +0530

    Add room entries and view function in room.py
    Inserted multiple room entries into the database and added a function to view all rooms with their details.

--- Hotel-Amenity-Management-System/room.py
+++ Hotel-Amenity-Management-System/room.py
@@ -1,1 +1,23 @@
+from db import rooms
 
+rooms.insert_many([
+    {"room_no":"101", "type":"single" , "price":1234},
+    {"room_no":"102", "type":"double" , "price":2345},
+    {"room_no":"103", "type":"triple" , "price":3456},
+    
+    {"room_no":"201", "type":"single" , "price":1456},
+    {"room_no":"202", "type":"double" , "price":2654},
+    {"room_no":"203", "type":"triple" , "price":3987},
+                
+    {"room_no":"301", "type":"single" , "price":1098},
+    {"room_no":"302", "type":"double" , "price":2987},
+    {"room_no":"303", "type":"triple" , "price":3789},     
+       ])
+
+def view_rooms():
+    all_rooms=rooms.find()
+
+    for i in all_rooms:
+        print("Room no.=",i["room_no"])
+        print("type of room =",i["type"])
+        print("Price=",i["price"],"\n")
